4576a15add025015eeaa9d9bc3d7a6f57cb6aab8
Naccter <henning.rydberg@elev.ga.lbs.se>
2024-05-26 23:08:20 +0200

    Många ändringar i javascript för det fungarade bättre såhär. jag kom på ett sätt att göra det så att man inte behöver ha 40 st onclick saker, det fungerar bra just nu ska bara fixa forecast grejerna. WOOO

--- Script/script.js
+++ Script/script.js
@@ -1,30 +1,56 @@
+document.getElementById("Refresh").onclick = WeatherRequest;
 
-WeatherRequest();
+document.querySelectorAll('.dropdown-content a').forEach(item => {
+    item.onclick = function(event) {
+        event.preventDefault(); // Prevent default link behavior
+        changeLocation(this.innerText);
+    }
+});
 
-
+function changeLocation(NewLocation) {
+    document.querySelector(".location").innerText = NewLocation;
+    WeatherRequest();
+}
 
 function WeatherRequest() {
-    var Location = document.getElementsByClassName("location")[0].innerHTML;
+    var Location = document.querySelector(".location").innerText;
     fetch(`http://api.weatherapi.com/v1/forecast.json?key=b2aa33b713b04af78c082210242404&q=${Location}&days=6&aqi=no&alerts=no`)
     .then(response => response.json())
     .then(data => {
         var currentTemp = data.current.temp_c;
         var currentTime = data.location.localtime;
-        
+        var currentDate = data.forecast.forecastday[0].date;
+        var chanceOfRain = data.forecast.forecastday[0].day.daily_chance_of_rain;
 
-        const temperature = document.getElementsByClassName("temperature")[0];
-        temperature.innerHTML = `${currentTemp}°C`;
-         
-        document.getElementsByClassName("current-time")[0].innerHTML = currentTime;
-        console.log(data);
+        const inputDate = currentDate;
+        const dayOfWeek = getDayOfWeek(inputDate);
+
+        function getDayOfWeek(dateString) {
+          const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+          const date = new Date(dateString); // Create a new Date object from the input date string
+          const dayOfWeek = date.getDay(); // Get the day of the week (0-6)
+        
+          return daysOfWeek[dayOfWeek];
+        }
+
+
+
+
+
+        document.querySelector(".rain-chance").innerHTML = `there is a ${chanceOfRain} % chance of rain.`
+        document.querySelector(".temperature").innerHTML = `${currentTemp}°C`;
+        document.querySelector(".current-time").innerHTML = currentTime;
+        document.querySelector(".current-day").innerHTML = dayOfWeek;
+
+
+
     })
     .catch(error => {
-      console.error('Error fetching weather data:', error);
+        console.error('Error fetching weather data:', error);
     });
-
-  }
-
+}
 
 
+  
 
-
+WeatherRequest();
