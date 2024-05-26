442d3ac5849e689dbad62322abea566f88fe6dd5
Naccter <henning.rydberg@elev.ga.lbs.se>
2024-05-26 15:58:08 +0200

    nytt

--- Script/script.js
+++ Script/script.js
@@ -1,23 +1,30 @@
 
+WeatherRequest();
 
 
-async function WeatherRequest() {
-    var Location = document.getElementById("Location");
-    await fetch("http://api.weatherapi.com/v1/forecast.json?key=b2aa33b713b04af78c082210242404&q=Seoul&days=6&aqi=no&alerts=no")
-    .then(response => response.json)
+
+function WeatherRequest() {
+    var Location = document.getElementsByClassName("location")[0].innerHTML;
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=b2aa33b713b04af78c082210242404&q=${Location}&days=6&aqi=no&alerts=no`)
+    .then(response => response.json())
     .then(data => {
-        const currentTemp = data.current.temp_c;
-        const currentTime = data.location.localtime;
+        var currentTemp = data.current.temp_c;
+        var currentTime = data.location.localtime;
+        
+
+        const temperature = document.getElementsByClassName("temperature")[0];
+        temperature.innerHTML = `${currentTemp}°C`;
          
+        document.getElementsByClassName("current-time")[0].innerHTML = currentTime;
+        console.log(data);
     })
-    console.log(Weather);
-    
+    .catch(error => {
+      console.error('Error fetching weather data:', error);
+    });
+
   }
 
 
 
 
 
-  window.onload = () => {
-    WeatherRequest();
-  }
